951a72774b3e8f0959f478a97fea38586f1528c2
Laura Mendieta <laumendieta06@gmail.com>
2024-07-11 21:35:09 -0500

    [UPDATE] FORMULARIOS REACTIVOS

--- fundamentos/src/app/app.component.ts
+++ fundamentos/src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // Se importa para poder usar logica en HTML
 import { CommonModule } from '@angular/common';
+// Para formularios driven-templates
+import { FormsModule } from '@angular/forms';
+// Para formularios reactivos
+// Grupos y controles
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -35,6 +41,10 @@ export class AppComponent {
   isDelete:boolean = false;
   isShowed:boolean = false;
   productos: string[] = ["galletas","cupcakes","muss de maracuya"];
+  // ----------------------------------------------------
+  // VARIABLES ASOCIADAS A FORMULARIOS
+  colorFavorito:string = "";
+  nombre:string = "";
   // USO DE ENLACE DE DATOS
 
   saludo(){
@@ -59,4 +69,33 @@ export class AppComponent {
   cambioShowed(){
     this.isShowed = !this.isShowed;
   }
+  // --------------------------------------------------------------
+  // FORMULARIOS DRIVEM-TEMPLATES
+
+  manejarEnvio(){
+    alert(`FORMULARIO DRIVEN-TEMPLATE
+      los datos del formulario son:
+       ${this.colorFavorito} - ${this.nombre}`);
+  }
+
+  // FORMULARIOS REACTIVOS
+  // 1- CREAR EL GRUPO (DEPENDE DE QUE HAGA EL FORMULARIO)
+  login = new FormGroup({
+      // 2. CREAR LOS CONTROLES (DEPENDEN DE LOS INPUTS QUE SE QUIERAN ALMACENAR)
+    correo : new FormControl(""),
+    contrasena : new FormControl("")
+
+  })
+
+  // 3. CREACION DE METODO
+
+  manejarEnvioLogin(){
+    // Los toma como objeto
+    console.log("Valores ingresados:", this.login.value );
+    // Tomar un dato en especifico
+    console.log("Valores ingresados:", this.login.get("correo")?.value );
+  }
+
+  // 4. CONFIGURAR EL ENVIO DESDE EL HTML (AGREGAR ATRIBUTOS O PROPIEDADES NECESARIAS EN FORMS)
+  
 }
