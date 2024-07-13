fa07a39c5bce74a074e2d8579ee10160e3705f1d
Laura Mendieta <laumendieta06@gmail.com>
2024-07-12 22:01:25 -0500

    [UPDATE] COMUNICACION PADRE - HIJO

--- fundamentos/src/app/components/componente-hijo/componente-hijo.component.ts
+++ fundamentos/src/app/components/componente-hijo/componente-hijo.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+// AQUI SE IMPORTA INPUT (ENTRADA) Y OUTPUT (SALIDA), EventEmitter (Para escuchar eventos)
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-componente-hijo',
@@ -20,5 +21,29 @@ export class ComponenteHijoComponent {
     edad : 0
   }
 
+  // ENVIAR INFORMACION DESDE EL HIJO
+  // SIEMPRE QUE SE PASE INFORMACION AL PADRE SE MANEJAN EVENTOS
+  /*
+    1. Crear el manejador de evento
+  */
+  //  ESTO ES UN CONSTRUCTOR
+  @Output() eventoEnviarNombre: EventEmitter<string> = new EventEmitter<string> ();
+
+  // 2. Creamos una funcion que use eventoEnviarNombre para emitir lo que mandaremos al padre
+
+  enviarNombre(){
+    this.eventoEnviarNombre.emit("Laura Sofia");
+  }
 
 }
+
+// EJERCICIO DE PRACTICA
+
+/**
+ 
+  - Desde el componente hijo mande al padre un objeto de datos
+  1.  El objeto debe tener nombre, caterogia e imagen
+  2. En el componente padre debe escuchar el objeto que se mande y mostrarlo
+  como una tarjeta.
+
+ */
--- fundamentos/src/app/components/componente-padre/componente-padre.component.ts
+++ fundamentos/src/app/components/componente-padre/componente-padre.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ComponenteHijoComponent } from '../componente-hijo/componente-hijo.component';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-componente-padre',
   standalone: true,
-  imports: [ComponenteHijoComponent],
+  imports: [ComponenteHijoComponent, CommonModule],
   templateUrl: './componente-padre.component.html',
   styleUrl: './componente-padre.component.css'
 })
@@ -17,5 +19,23 @@ export class ComponentePadreComponent {
     apellido:"Mendieta",
     numeroCedula:1033683621,
     edad:50
+  }
+
+  // PREPARAR AL PADRE PARA RECIBIR
+  /**
+   1. Crear una variable donde vamos a recibir la informacion
+   2. Crear un metodo para guardar los datos recibidos
+   * 
+  */
+
+  nombreRecibido : string = "";
+  nombresRecibidos : string[] = [];
+  // Esta funcion me recibe el parametro enviado por el hijo
+  recibirNombre(nombre : string){
+
+    this.nombreRecibido = nombre;
+    // Si quiero que persista la informacion se debe llamar a la peticion post
+    this.nombresRecibidos.push(nombre);
+
   }
 }
